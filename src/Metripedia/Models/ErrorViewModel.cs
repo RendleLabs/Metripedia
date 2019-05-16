1a0a895f5658ec9527f0364a97266a6a5f753b98
Mark Rendle <mark@rendlelabs.com>
2019-05-16 10:51:23 +0100

    New project

---
+++ src/Metripedia/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Metripedia.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
